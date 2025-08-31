a3b2656cf79a84d896496f19956b206355b7a278
Natalia <nataliaskorupa.2003@gmail.com>
2025-08-31 12:47:24 +0200

    refactor: Remove Page component wrapping from App; simplify Outlet rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import * as React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
-import Page from '../app.js';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
-        <Page>
-          <Outlet />
-        </Page>
+        <Outlet />
       </main>
       <Footer />
     </div>
