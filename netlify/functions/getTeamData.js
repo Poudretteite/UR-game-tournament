a759577c53c85949bcb080dc1d11a3d6bbcb745b
Natalia <nataliaskorupa.2003@gmail.com>
2025-09-02 16:16:40 +0200

    feat: Implement team data export functionality with JSON and CSV formats, and add necessary dependencies

--- netlify/functions/getTeamData.js
+++ netlify/functions/getTeamData.js
@@ -1,4 +1,6 @@
 import { Pool } from "pg";
+import Papa from "papaparse";
+import JSZip from "jszip";
 
 const pool = new Pool({
     connectionString: process.env.NETLIFY_DATABASE_URL,
@@ -7,6 +9,11 @@ const pool = new Pool({
 
   export async function handler(event) {
   const token = event.headers['x-admin-token'];
+
+  if (event.httpMethod !== "GET") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
   if (token !== process.env.ADMIN_API_KEY) {
     return {
       statusCode: 401,
@@ -14,5 +21,40 @@ const pool = new Pool({
     };
   }
 
-  
+  const client = await pool.connect();
+  try {
+    const result = await client.query(`
+      SELECT 
+        t.TeamName, t.CaptainName, t.CaptainTel, t.CaptainEmail, 
+        m.FirstName, m.LastName, m.Steam, m.BirthDate, m.ShirtSize
+      FROM Teams t
+      JOIN Members m ON t.id = m.TeamID
+      ORDER BY t.id, m.id
+    `);
+
+    const jsonString = JSON.stringify(result.rows, null, 2);
+    const csv = Papa.unparse(result.rows);
+
+    const zip = new JSZip();
+    zip.file("team_data.json", jsonString);
+    zip.file("team_data.csv", csv);
+
+    const zipContent = await zip.generateAsync({ type: "base64" });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/zip",
+        "Content-Disposition": "attachment; filename=team_data.zip",
+      },
+      isBase64Encoded: true,
+      body: csv
+    };
+
+  } catch (err) {
+    console.error("Błąd:", err);
+    return { statusCode: 500, body: err.message };
+  } finally {
+    client.release();
+  }
 }
