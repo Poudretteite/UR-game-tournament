388f477203a112c7fdb6228b25c16f17182f76c3
Natalia <nataliaskorupa.2003@gmail.com>
2025-10-13 18:35:27 +0200

    fix: correct typo in Thank You message

--- src/routes/ThankYou.jsx
+++ src/routes/ThankYou.jsx
@@ -10,7 +10,7 @@ const ThankYou = () => {
                     <div className='flex flex-col py-10 md:pt-32 mb-10 space-y-6 p-6 h-a bg-black bg-opacity-30 rounded-2xl text-center shadow-[inset_2px_2px_15px_#1952ff]'>
                     <h2 className="text-6xl font-semibold py-3 text-white italic">Dziękujemy za rejestrację!</h2>
                     <h2 className="text-3xl font-semibold text-white">Formularz został wysłany pomyślnie.</h2>
-                    <h3 className="text-xl text-white">Potwiedzimy twoje zgłoszenie na podany adres e-mail.</h3>
+                    <h3 className="text-xl text-white">Potwierdzimy twoje zgłoszenie na podany adres e-mail.</h3>
                 </div>
             </main>
         </>
