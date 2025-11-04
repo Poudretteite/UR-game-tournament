868882a8f53dce293bbad6d20b8cdb1edb295b79
Natalia <nataliaskorupa.2003@gmail.com>
2025-11-04 18:46:27 +0100

    fix: add Twitch link to footer for event promotion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BsFacebook } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
+import { BsTwitch } from "react-icons/bs";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
@@ -8,6 +9,14 @@ const Footer = () => {
     return (
         <footer className="bg-[#0a1b3e]">
             <div className=" flex flex-wrap items-center justify-center gap-4 pt-5">
+                <a
+                    href="https://www.twitch.tv/urgamingtournament"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center justify-center rounded-full shadow-md transition hover:scale-110"
+                >
+                    <BsTwitch className='text-white text-3xl'/>
+                </a>
                 <a
                     href="https://www.facebook.com/events/1464955321456546"
                     target="_blank"
