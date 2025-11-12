3f086c1bc67f4328994add481428fc3818c04ac8
Kasia Reczek <kasiar2332@gmail.com>
2025-11-12 08:34:06 +0100

    show stream

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
                     <section>
                         <h2 className={h2}>Finał - LAN</h2>
                         <h3 className={h3}>12 Listopada</h3>
-                        <h3 className={h3}>Zen.com Expo 8:00 - 16:00</h3>
+                        <h3 className={h3}>Zen.com Expo 8:00 - 16:00 </h3>
                         <h3 className={h3}>Wszystkie informacje o festiwalu: 
                             <br /><a href='https://infopack.pfni.pl' className='underline' target='blank'>PFNI Infopack</a>
                         </h3>
